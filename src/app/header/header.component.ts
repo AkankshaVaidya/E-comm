e24fd4b4faea97f900c3d08e5b9d87eb85dc81a3
Akanksha <akankshavaidya1999@gmail.com>
2024-12-25 17:30:30 +0530

    Added bootstrap,worked on displaying list

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from '@angular/router'
+import { product } from '../data-type';
+import { ProductService } from '../services/product.service';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -7,10 +9,11 @@ import {Router} from '@angular/router'
 })
 export class HeaderComponent implements OnInit{
 
-  constructor(private route:Router) { }
+  constructor(private route:Router,private product:ProductService) { }
 
    menuType:String='default';
    sellerName:string='';
+   searchResult:undefined | product[];
 
   ngOnInit(): void {
     this.route.events.subscribe((val:any)=>{
@@ -34,6 +37,24 @@ export class HeaderComponent implements OnInit{
   logout(){
     localStorage.removeItem('seller');
     this.route.navigate(['/']);
+  }
+
+  searchProduct(query:KeyboardEvent){
+    if(query){
+      const element =query.target as HTMLInputElement;
+      this.product.searchProduct(element.value).subscribe((result)=>{
+        console.warn(result);
+        if(result.length>5){
+             result.length=5;
+        }
+        
+        this.searchResult=result;
+      } )
+    }
+  }
+
+  hideSearch(){
+    this.searchResult=undefined;
   }
 
 }
