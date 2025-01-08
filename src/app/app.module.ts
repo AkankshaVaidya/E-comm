b87253e3f39acc1595279ce14cab61a5c0d19be3
Akanksha <akankshavaidya1999@gmail.com>
2025-01-08 14:49:14 +0530

    ECOMM-Added cart page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,6 +18,7 @@ import { SearchComponent } from './search/search.component';
 import { FooterComponent } from './footer/footer.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
 import { UserAuthComponent } from './user-auth/user-auth.component';
+import { CartPageComponent } from './cart-page/cart-page.component';
 
 @NgModule({
   declarations: [
@@ -32,7 +33,8 @@ import { UserAuthComponent } from './user-auth/user-auth.component';
     SearchComponent,
     FooterComponent,
     ProductDetailsComponent,
-    UserAuthComponent
+    UserAuthComponent,
+    CartPageComponent
   ],
   imports: [
     BrowserModule,
