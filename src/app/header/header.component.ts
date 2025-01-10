9f8a25acfce8c33e503af0a4336cdf16986c484d
Akanksha <akankshavaidya1999@gmail.com>
2025-01-10 17:10:23 +0530

    fomatting

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {Router} from '@angular/router'
+import { Router } from '@angular/router'
 import { product } from '../data-type';
 import { ProductService } from '../services/product.service';
 @Component({
@@ -7,81 +7,81 @@ import { ProductService } from '../services/product.service';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit{
+export class HeaderComponent implements OnInit {
 
-  constructor(private route:Router,private product:ProductService) { }
+  constructor(private route: Router, private product: ProductService) { }
 
-   menuType:String='default';
-   sellerName:string='';
-   userName:string='';
-   searchResult:undefined | product[];
+  menuType: String = 'default';
+  sellerName: string = '';
+  userName: string = '';
+  searchResult: undefined | product[];
   cartItems = 0;
 
   ngOnInit(): void {
-    this.route.events.subscribe((val:any)=>{
-    if(val.url){
-      if(localStorage.getItem('seller') && val.url.includes('seller')){
-        let sellerStore=localStorage.getItem('seller');
-        let sellerData =  sellerStore && JSON.parse(sellerStore)[0];
-        this.sellerName=sellerData.name;
-        this.menuType='seller';
-      } else if(localStorage.getItem('user')){
-        let userStore=localStorage.getItem('user');
-        let userData =  userStore && JSON.parse(userStore);
-        this.userName=userData.name;
-        this.menuType='user';
-        this.product.getCartList(userData.id);
+    this.route.events.subscribe((val: any) => {
+      if (val.url) {
+        if (localStorage.getItem('seller') && val.url.includes('seller')) {
+          let sellerStore = localStorage.getItem('seller');
+          let sellerData = sellerStore && JSON.parse(sellerStore)[0];
+          this.sellerName = sellerData.name;
+          this.menuType = 'seller';
+        } else if (localStorage.getItem('user')) {
+          let userStore = localStorage.getItem('user');
+          let userData = userStore && JSON.parse(userStore);
+          this.userName = userData.name;
+          this.menuType = 'user';
+          this.product.getCartList(userData.id);
+        }
+        else {
+          this.menuType = 'default';
+        }
       }
-      else{
-        this.menuType='default';
-      }
-    }
     });
 
     let cartData = localStorage.getItem('localCart');
-    if(cartData){
-      this.cartItems=JSON.parse(cartData).length; 
+    if (cartData) {
+      this.cartItems = JSON.parse(cartData).length;
     }
 
-    this.product.cartData.subscribe((items)=>{
-      this.cartItems=items.length;
+    this.product.cartData.subscribe((items) => {
+      this.cartItems = items.length;
     });
   }
 
-  logout(){
+  logout() {
     localStorage.removeItem('seller');
     this.route.navigate(['/']);
     this.product.cartData.emit([]);
   }
 
-  userLogout(){ 
+  userLogout() {
     localStorage.removeItem('user');
     this.route.navigate(['/user-auth']);
   }
 
-  searchProduct(query:KeyboardEvent){
-    if(query){
-      const element =query.target as HTMLInputElement;
-      this.product.searchProduct(element.value).subscribe((result)=>{
-       
-        if(result.length>5){
-             result.length=5;
+  searchProduct(query: KeyboardEvent) {
+    if (query) {
+      const element = query.target as HTMLInputElement;
+      this.product.searchProduct(element.value).subscribe((result) => {
+
+        if (result.length > 5) {
+          result.length = 5;
         }
-        
-        this.searchResult=result;
-      } )
+
+        this.searchResult = result;
+      })
     }
   }
 
-  hideSearch(){
-    this.searchResult=undefined;
+  hideSearch() {
+    this.searchResult = undefined;
   }
-  
-  submitSearch(val:string){
+
+  submitSearch(val: string) {
     this.route.navigate([`search/${val}`]);
   }
 
-  redirectToDetails(id:number){
-    this.route.navigate(['/details/'+id]);
+  redirectToDetails(id: number) {
+    this.route.navigate(['/details/' + id]);
   }
 }
